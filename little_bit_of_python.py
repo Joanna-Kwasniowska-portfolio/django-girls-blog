0d45785f850f817f3b751d20a2d952902791af85
JoannaAgnieszka <jkwasniowska@gmail.com>
2023-10-07 12:21:02 +0200

    My Django Girls app, first commit

---
+++ little_bit_of_python.py
@@ -0,0 +1,50 @@
+# my_list =[2,43,12,67,33]
+# print(my_list)
+# my_list.sort()
+# print(my_list)
+# my_list.sort()
+# print(my_list)
+# my_list.append("Joanna")
+# print(my_list)
+# print(my_list[-1])
+# participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+# print(participant["name"])
+# len(participant)
+# participant["favorite_language"] = "Python"
+# print(participant)
+# participant.pop("favorite_numbers")
+# print(participant)
+# participant['country'] = 'Germany'
+# print(participant)
+
+# if 3 > 2:
+#     print("3 is indeed greater than 2")
+# else:
+#     print("5 is not greater than 2")
+
+# name = input("What is your name?: ")
+
+# def hi():
+#     if name == "Nadine":
+#         print("Hi Nadine :)")
+#     elif name == "Katharina":
+#         print("Nice to meet you, Katharina")
+#     elif name == "Fiona":
+#         print("Welcome Fiona")
+#     else:
+#         print(name, "register yourself, please")
+#
+# hi()
+
+def hi(name):
+    print("Hi" + name + "!")
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+    hi(name)
+    print("Nice to meet you, next one, please")
+
+
+
+
